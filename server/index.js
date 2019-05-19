2b8b349fb40f508e6654efa5ac1d2cb4b496a64a
jon cannon <iamjoncannon@gmail.com>
2019-05-19 11:01:00 -0400

    switched endpoints

--- server/index.js
+++ server/index.js
@@ -11,17 +11,29 @@ app.use(express.urlencoded({ extended: true }))
 app.set('trust proxy', true)
 // static middleware
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 
 	const ip = req.headers['x-real-ip'] 
 	console.log('ip of request ', req.headers['x-real-ip'])
 	const geoloc = require('geoip-lite').lookup(ip) 
 	console.log('visited from ', geoloc.city, geoloc.region, geoloc.country)
-	const textMessage = `visited from ${ip}: ${geoloc.city}, ${geoloc.region}, ${geoloc.country}`
+	const textMessage = `counter: ${ip}: ${geoloc.city}, ${geoloc.region}, ${geoloc.country}`
 	sendText(textMessage)
 
-  res.end()
-}) // Send index.html for any other requests
+	res.end()
+}) 
+
+app.get('/homepage', (req, res) => {
+
+	const ip = req.headers['x-real-ip'] 
+	console.log('ip of request ', req.headers['x-real-ip'])
+	const geoloc = require('geoip-lite').lookup(ip) 
+	console.log('visited from ', geoloc.city, geoloc.region, geoloc.country)
+	const textMessage = `portfolio: ${ip}: ${geoloc.city}, ${geoloc.region}, ${geoloc.country}`
+	sendText(textMessage)
+
+	res.end()
+}) 
 
 // error handling middleware
 app.use((err, req, res, next) => {
